f9bbf57512ea1783b08c2be64dcb04fe350537c3
Joe Richter <joerichterdev@gmail.com>
2017-06-01 21:43:21 -0400

    Edit Device Specific Themes
    Chnaged the send button for iOS vs Android and some coloring schemes

--- lib/chat/home.dart
+++ lib/chat/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'chat_message.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -17,25 +18,27 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
       appBar: new AppBar(
         title: new Text("Flutter Chat"),
       ),
-      body: new Column(children: <Widget>[
-        new Flexible(
-          child: new Container(
-            child: new ListView.builder(
-              itemCount: _messages.length,
-              itemBuilder: (_, int index) => _messages[index],
-              reverse: true,
+      body: new Container(
+        child: new Column(children: <Widget>[
+          new Flexible(
+            child: new Container(
+              child: new ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (_, int index) => _messages[index],
+                reverse: true,
+              ),
+              padding: new EdgeInsets.all(10.0),
             ),
-            padding: new EdgeInsets.all(10.0),
           ),
-        ),
-        new Divider(height: 1.0),
-        new Container(
-          decoration: new BoxDecoration(
-            color: Theme.of(context).cardColor,
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
           ),
-          child: _buildTextComposer(),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 
@@ -62,13 +65,20 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
           ),
         ),
         new Container(
-          child: new IconButton(
-            icon: new Icon(Icons.send),
-            color: Theme.of(context).accentColor,
-            onPressed: _isComposing ?
-              () => _handleMessageSubmit(_textController.text) :
-              null,
-          ),
+          child: Theme.of(context).platform == TargetPlatform.iOS
+              ? new CupertinoButton(
+                  child: new Text("Send"),
+                  onPressed: _isComposing
+                      ? () => _handleMessageSubmit(_textController.text)
+                      : null,
+                )
+              : new IconButton(
+                  icon: new Icon(Icons.send),
+                  color: Theme.of(context).accentColor,
+                  onPressed: _isComposing
+                      ? () => _handleMessageSubmit(_textController.text)
+                      : null,
+              ),
         ),
       ]),
     );
